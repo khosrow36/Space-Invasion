1dfd29679fa3542c82140916377f4d749ad9e2f4
Arti <artek.przystas@gmail.com>
2019-11-24 21:47:45 +0100

    code improvements

--- space-invasion/game_functions.py
+++ space-invasion/game_functions.py
@@ -1,22 +1,28 @@
 import sys
 import pygame
 
+def check_keydown_events(event, ship):
+        if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+            ship.moving_right = True
+        elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
+            ship.moving_left = True
+
+def check_keyup_events(event, ship):
+        if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+            ship.moving_right = False
+        elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
+            ship.moving_left = False
+
 def check_events(ship):
     """Check keyboard/mouse events"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
+
 
 def update_screen(settings, screen, ship):
     """Update the screen"""
